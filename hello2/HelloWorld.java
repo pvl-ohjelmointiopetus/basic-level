549571e0584226518a8f42dbff35acc722658ac9
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-07-01 22:52:37 +0300

    Kommentteja

--- hello2/HelloWorld.java
+++ hello2/HelloWorld.java
@@ -1,9 +1,13 @@
+// Huomaa: Tämä on HelloWorld, koska tiedoston nimi on HelloWorld.java 
 public class HelloWorld {
 
 	public static void main(String args[]) {
+
+		// luodaan muuttujia
 		String name = "Matti";
 		int thisYear = 2011;
 		int birthYear = 1988;
+
 		System.out.println("Hei, nimeni on " + name + " ja olen " + ( thisYear - birthYear ) + " vuotta vanha" );
 	}
 
