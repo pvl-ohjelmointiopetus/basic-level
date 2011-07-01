8a653e112e9d12512fa05b8e540f7aed3905e2b8
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-07-01 21:02:20 +0300

    Lisätty huomautus tiedoston nimestä

--- hello1/HelloWorld.java
+++ hello1/HelloWorld.java
@@ -1,7 +1,11 @@
+// Huomaa: Tämä on HelloWorld, koska tiedoston nimi on HelloWorld.java
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
+
 		System.out.println("Hei, nimeni on Matti ja opiskelen ohjelmoimaan!");
+
 	}
 
 }
