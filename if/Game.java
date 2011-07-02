53c4ded51f2d66dacb191efc4e3ea71d53da44e9
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-07-02 16:29:24 +0300

    Lisätty tehtäviä

--- if/Game.java
+++ if/Game.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+// Nimi Game tulee siitä, että on Game.java
 public class Game {
 
 	public static void main(String[] args) {
@@ -11,6 +12,13 @@ public class Game {
 		int guess = read.nextInt();
 
 		int computer = 10;
+
+		// TODO 1: Siirrä voitto / häviötekstit muuttujiin
+		// Niitä on helpompi muokata silloin
+
+		// TODO 2: Kerro voitto/häviötekstissä, että minkä luvun arvasit ja mitä lukua tietokone arvasi
+
+		// TODO 3: Toteuta huijaustila: tietokone arvaa aina yhden luvun isomman luvun kuin käyttäjä
 
 		if( guess < computer ) {
 			System.out.println("Minä voitin!");
